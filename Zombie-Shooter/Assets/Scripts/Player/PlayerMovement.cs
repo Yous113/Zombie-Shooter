f92b4aa645fd0d9a9ab586529486f57da2418cf7
Youssef <youssef.n6@gmail.com>
2018-12-05 19:07:09 +0100

    movement and cameracontrol

--- Zombie-Shooter/Assets/Scripts/Player/PlayerMovement.cs
+++ Zombie-Shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = 6f;
+    //public float gravity = -9,8f;
+
+    private CharacterController _charCont;
+
+     void Start()
+    {
+        _charCont = GetComponent<CharacterController>();
+    }
+
+     void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed); //limits the max speed of the player
+
+        //movement.y = gravity;
+        
+
+        movement *= Time.deltaTime; // time.deltaTime Ensures the speed the player moves does not change based on frame rate
+        movement = transform.InverseTransformDirection(movement);
+        _charCont.Move(movement);
+    }
 }
---
+++ Zombie-Shooter/Assets/Scripts/Zombie/EnemyFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+    public float speed;
+    private Transform target;
+
+     void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
