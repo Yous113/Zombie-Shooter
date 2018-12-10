a2443361e53b4f8fa36eb80166d17e6c01ad7c95
Youssef <youssef.n6@gmail.com>
2018-12-10 19:26:27 +0100

    Wave Spawner Script

--- Zombie-Shooter/Assets/Scripts/WaveSpawner/WaveSpawner.cs
+++ Zombie-Shooter/Assets/Scripts/WaveSpawner/WaveSpawner.cs
@@ -1,16 +1,64 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum SpawnState {SPAWNING, WAITING, COUNTING};
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform Enemy;
+        public int amount;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+     void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
+     void Update()
+    {
+        if (waveCountdown <= 0)
+        {
+           if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine( SpawnWave ( waves [ nextWave ]));
+            }
+            else
+            {
+                waveCountdown -= Time.deltaTime;
+            }
+        }
+    }
+
+    IEnumerator SpawnWave (Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.amount; i++)
+        {
+            SpawnEnemy(_wave.Enemy);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy (Transform _enemy)
+    {
+        //Spawn enemy
+        Debug.Log("Spawning Enemy" + _enemy.name);
+    }
 }
